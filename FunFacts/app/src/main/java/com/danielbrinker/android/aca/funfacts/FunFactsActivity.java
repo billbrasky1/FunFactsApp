b3a9cdd25c6f479bb3c3e738aa6293fc3e37f81d
Daniel Brinker <danieldeanbrinker@gmail.com>
2017-01-25 13:53:32 -0600

    FunFacts: practice with arrays and dynamic XML layouts.

--- FunFacts/app/src/main/java/com/danielbrinker/android/aca/funfacts/FunFactsActivity.java
+++ FunFacts/app/src/main/java/com/danielbrinker/android/aca/funfacts/FunFactsActivity.java
@@ -1,13 +1,51 @@
 package com.danielbrinker.android.aca.funfacts;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
+    //get simple name leaves the package name out and just gives the class name
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+
+    // declare our View member variables
+    private TextView mFactTextView;
+    private Button mShowFactButton;
+    private RelativeLayout mRelativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        //Asign the views from the layout files to the corresponding variables
+        mFactTextView = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.activity_fun_facts);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fact = mFactBook.getFact();
+                int color = mColorWheel.getColor();
+
+                //Update the screen with our dynamic fact
+                mFactTextView.setText(fact);
+                mRelativeLayout.setBackgroundColor(color);
+                mShowFactButton.setTextColor(color);
+            }
+        };
+        mShowFactButton.setOnClickListener(listener);
+
+        // Toast.makeText(FunFactsActivity.this, "Yay! Our Activity was created!", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "We're logging from the OnCreate() method!");
+
     }
+
 }
